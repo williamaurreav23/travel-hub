29e894cd18ecf8fb5dc331946fea0db13f9e1506
mithulix <mohaiminul49@gmail.com>
2021-06-25 17:58:29 +0600

    new

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function  Home () {
+    return (
+        <div>
+            <h3>home</h3>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corporis mollitia odit nihil dolorum assumenda, illo quidem omnis odio exercitationem dolor harum quo nemo dicta, quia pariatur quos. Eligendi, iste?
+        </div>
+    );
+};
